951db53a148fcd17fbae923bdd575ed7f4a3e344
Kuang Yu Heng <yuheng3107@gmail.com>
2023-07-16 10:59:00 +0800

    Made rudimentary function to generate noisy data from predefined centroids

--- src/components/KMeansVisualization.tsx
+++ src/components/KMeansVisualization.tsx
@@ -1,40 +1,22 @@
-import {
-  ScatterChart,
-  Scatter,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  ResponsiveContainer,
-  ReferenceDot,
-  Cross,
-} from "recharts";
-
-interface Point {
-  x: number;
-  y: number;
-}
-
-interface ScatterPlotProps {
-  centroid1: Point;
-  centroid2: Point;
-  points1: Point[];
-  points2: Point[];
-}
-export default function ScatterPlot({
-  centroid1,
-  centroid2,
-  points1,
-  points2,
-}: ScatterPlotProps) {
-  <ResponsiveContainer width="100%" height="100%">
-    <ScatterChart>
-      <CartesianGrid />
-      <XAxis />
-      <YAxis />
-      <Scatter name="Points 1" data={points1} fill="#dc2626" />
-      <Scatter name="Points 2" data={points2} fill="#0284c7" />
-      <ReferenceDot {...centroid1} fill="#dc2626" shape={<Cross />} />
-      <ReferenceDot {...centroid2} fill="#0284c7" shape={<Cross />} />
-    </ScatterChart>
-  </ResponsiveContainer>;
+import ScatterPlot from "./ScatterPlot";
+import { Point } from "./ScatterPlot";
+import { useState } from "react";
+export default function KMeansVisualization() {
+  const [points1, setPoints1] = useState({});
+  const [points2, setPoints2] = useState({});
+  const generateData = (centroid1: Point, centroid2: Point, n: number) => {
+    // function adds noise to both centroids to generate n data points for each centroid
+    let noisyPoints1 = Array.from({ length: n }, () => {
+      let x = centroid1.x + Math.random();
+      let y = centroid1.y + Math.random();
+      return { x, y };
+    });
+    let noisyPoints2 = Array.from({ length: n }, () => {
+      let x = centroid2.x + Math.random();
+      let y = centroid2.y + Math.random();
+      return { x, y };
+    });
+    setPoints1(noisyPoints1);
+    setPoints2(noisyPoints2);
+  };
 }
--- src/components/ScatterPlot.tsx
+++ src/components/ScatterPlot.tsx
@@ -1,15 +1,40 @@
-import * as d3 from "d3";
+import {
+  ScatterChart,
+  Scatter,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  ResponsiveContainer,
+  ReferenceDot,
+  Cross,
+} from "recharts";
 
-type ScatterPlotProps = {
-  width: number;
-  height: number;
-  data: { x: number; y: number }[];
-};
+interface Point {
+  x: number;
+  y: number;
+}
 
-export default function ScatterPlot({ width, height, data }: ScatterPlotProps) {
-  return (
-    <div>
-      <svg width={width} height={height}></svg>
-    </div>
-  );
+interface ScatterPlotProps {
+  centroid1: Point;
+  centroid2: Point;
+  points1: Point[];
+  points2: Point[];
+}
+export default function ScatterPlot({
+  centroid1,
+  centroid2,
+  points1,
+  points2,
+}: ScatterPlotProps) {
+  <ResponsiveContainer width="100%" height="100%">
+    <ScatterChart>
+      <CartesianGrid />
+      <XAxis />
+      <YAxis />
+      <Scatter name="Points 1" data={points1} fill="#dc2626" />
+      <Scatter name="Points 2" data={points2} fill="#0284c7" />
+      <ReferenceDot {...centroid1} fill="#dc2626" shape={<Cross />} />
+      <ReferenceDot {...centroid2} fill="#0284c7" shape={<Cross />} />
+    </ScatterChart>
+  </ResponsiveContainer>;
 }
